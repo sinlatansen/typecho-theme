4e1022a270d436daeff3b1d4c28757afa3c5beef
lzy <lllzzzyyy@buaa.edu.cn>
2024-02-26 18:01:39 +0800

    modified:   .gitignore 	new file:   public/config.php 	modified:   public/fetchUmamiData.php

---
+++ public/config.php
@@ -0,0 +1,8 @@
+// config.php
+return [
+    'loginUrl' => 'http://umami.lzyyyyyy.fun/api/auth/login',
+    'activeUsersUrl' => 'http://umami.lzyyyyyy.fun/api/websites/7aa963db-7032-4a0e-a823-bbda16a88221/active',
+    'statsUrlBase' => 'http://umami.lzyyyyyy.fun/api/websites/7aa963db-7032-4a0e-a823-bbda16a88221/stats',
+    'username' => 'admin',
+    'password' => 'your_secure_password',
+];
